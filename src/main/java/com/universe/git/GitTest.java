e33498dbc46dafa163a6c67ab642bf78d727d5be
universe-ysf <15737530067@163.com>
2021-05-15 21:35:28 +0800

    first commit

---
+++ src/main/java/com/universe/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.universe.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
